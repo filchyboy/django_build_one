bce7c77c96eeb445d13763cc9e0d60a638b67d42
Your name hereChristopher <christopher.filkins@gmail.com>
2020-04-01 13:50:47 -0700

    Added administration functionality and created initial models

--- django_build_one/blog/views.py
+++ django_build_one/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'PersonA',
-        'title': 'Post 1',
-        'content': 'Content',
-        'date_posted': 'March 31, 2020'
-    },
-    {
-        'author': 'PersonB',
-        'title': 'Post 2',
-        'content': 'More Content',
-        'date_posted': 'April 1, 2020'
-    }
-    
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
