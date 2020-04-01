604c359b938ee1c0eaf9726326f7b96846b6aa0a
Your name hereChristopher <christopher.filkins@gmail.com>
2020-04-01 10:25:51 -0700

    Installed bootstrap, templates, bower, npm, jquery

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # django_build_one
 Learning how to use django
+
+Use:
+python manage.py runserver
--- django_build_one/blog/views.py
+++ django_build_one/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author': 'PersonA',
+        'title': 'Post 1',
+        'content': 'Content',
+        'date_posted': 'March 31, 2020'
+    },
+    {
+        'author': 'PersonB',
+        'title': 'Post 2',
+        'content': 'More Content',
+        'date_posted': 'April 1, 2020'
+    }
+    
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About Django Enterprises'})
